b1806724b8df9b967959d90f07898a2e0dc0a41b
anchella <rajinikanth.anchella@gmail.com>
2024-08-06 09:20:03 +0530

    added code in spec file

--- tests/first_script.spec.js
+++ tests/first_script.spec.js
@@ -2,14 +2,15 @@ import { test, expect } from "@playwright/test";
 // const { test, expect } = require("@playwright/test");
 test("Open google page", async ({ page }) => {
   //Navigate to page
-  // await page.goto(
-  //   "https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php"
-  // );
-  // await expect(page).toHaveTitle(
-  //   "Selenium Practice - Student Registration Form"
-  // );
+  await page.goto(
+    "https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php"
+  );
+  await expect(page).toHaveTitle(
+    "Selenium Practice - Student Registration Form"
+  );
 
-  await page.goto("https://www.google.com/");
+  // await page.goto("https://www.google.com/");
+  await page.locator("css=#name").isVisible;
   // const googleSearch = page.getByRole("textarea", { class: "gLFyf" });
   // await googleSearch.click();
   // await page.getByRole("")
