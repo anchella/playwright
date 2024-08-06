11a689c1fe5bbc5a43285e9c55c54c52d66582fc
anchella <rajinikanth.anchella@gmail.com>
2024-08-06 09:40:29 +0530

    Added base URL and removed unwanted code

--- tests/first_script.spec.js
+++ tests/first_script.spec.js
@@ -2,15 +2,20 @@ import { test, expect } from "@playwright/test";
 // const { test, expect } = require("@playwright/test");
 test("Open google page", async ({ page }) => {
   //Navigate to page
-  await page.goto(
-    "https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php"
-  );
+  await page.goto("/selenium/practice/selenium_automation_practice.php");
   await expect(page).toHaveTitle(
     "Selenium Practice - Student Registration Form"
   );
 
   const registrationName = page.locator("css=#name");
+  const registrationEmail = page.locator("css=#email");
+
   expect(registrationName.isVisible).toBeTruthy;
   await registrationName.fill("Rajinikanth");
-  await expect(registrationName).toHaveText("Rajinikanth");
+
+  await registrationEmail.waitFor();
+  await registrationEmail.fill("test@gmail.com");
+
+  // await page.pause();
+  // await expect(registrationName).toHaveText("Rajinikanth");
 });
